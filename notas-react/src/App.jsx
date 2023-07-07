6a012b8349b039049d026f3393707df4bd4e9b11
Jonika3000 <66825034+Jonika3000@users.noreply.github.com>
2023-07-07 17:23:20 +0300

    Add Login Logic And Page

--- notas-react/src/App.jsx
+++ notas-react/src/App.jsx
@@ -3,10 +3,11 @@ import { Route, Routes } from 'react-router-dom'
 import DefaultLayout from './components/default-layout/default-layout.jsx'
 import HomePage from './components/home-page/home-page.jsx'
 import LoginPage from './components/login-page/login-page.jsx'
+import { Helmet } from 'react-helmet'
 
 function App() { 
   return (
-    <>
+    <>  
     <Routes>
         <Route path="/" element={<DefaultLayout/>}>
            <Route index element={<HomePage/>}/>
--- notas-react/src/components/login-page/login-page.jsx
+++ notas-react/src/components/login-page/login-page.jsx
@@ -1,37 +1,58 @@
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import './login-page.css'
+import { useState } from 'react';
+import axios from '../../api/axios';
+import { useNavigate } from 'react-router-dom';
 
-const LoginPage = ()=>{
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate(); 
+   
+    const handleSubmit = async (event) => {
+        event.preventDefault(); 
+        try { 
+           await axios.post('/login',{email,password});
+           setEmail("");
+           setPassword("");
+            navigate("/");
+        }
+        catch (error) {
+            console.log(error);
+        }  
     };
-    return(
+ 
+    return (
         <div className="LoginWrapper">
             <div className='LoginBox'>
                 <div>
                     <h1>Welcome, again</h1>
-                    <Form onSubmit={handleSumbit}>
+                    <Form onSubmit={handleSubmit}> 
                         <Form.Group>
                             <Form.Label>Email</Form.Label>
                             <Form.Control
-                            type='text'
-                            placeholder='enter email'
-                            name='email'
-                            required>
+                                type='text'
+                                placeholder='enter email'
+                                name='email'
+                                value={email}
+                                onChange={(e)=> setEmail(e.target.value)}
+                                required>
                             </Form.Control>
-                        </Form.Group>
+                        </Form.Group> 
                         <Form.Group>
                             <Form.Label>Password</Form.Label>
                             <Form.Control
                                 type='password'
                                 placeholder='enter password'
                                 name='password'
-                                required>
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}>
                             </Form.Control>
                         </Form.Group>
+                        <Button type='submit'>Login</Button>
                     </Form>
-                </div> 
+                </div>
             </div>
         </div>
     );
